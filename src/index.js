4910eba1be3e6551250f3e1ca9964207ab0a3f12
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-20 19:46:09 +0200

    updaete debounce

--- src/index.js
+++ src/index.js
@@ -12,29 +12,27 @@ const countryContainer = document.querySelector('.country-info')
 const DEBOUNCE_DELAY = 300;
 
 
-
-
-
-// fetchCountries('sw').then(data => { console.log(data)}).catch()
-      function createCountryMarkup(data) {
-    const markup = data.map(item =>`<li class="country-item"><img class="country-img" src="${item.flags.svg}" alt="" width=30><p>${item.name.official}</p></li>`).join('')
-        // console.log(markup)
-       listEl.innerHTML = markup
-      }
-   function createCountryInfoMarkup(data) {
+    function createCountryMarkup(data) {
+    const markup = data.map(item =>`<img class="country-img" src="${item.flags.svg}" alt="" width=30><p>${item.name.official}</p>`).join('')
+       
+    listEl.innerHTML = markup
+}
+      
+    function createCountryInfoMarkup(data) {
     const markup = data.map(item =>`<img class="country-info-img" src="${item.flags.svg}" alt="" width=30>
       <p class="country-info-title">${item.name.official}</p>
       <p class="country-info-heading">Capital:${item.capital}</p>
       <p class="country-info-heading">Population:${item.population}</p>
       <p class="country-info-heading">Languages:${Object.values(item.languages)}</p>`).join('')
+        
      countryContainer.innerHTML = markup
 }
 
 const checkCountriesHandler = (event) => {
     const name = event.target.value
-    // console.log(name)
     
-    fetchCountries(name).then(data => {
+    
+    fetchCountries(name.trim()).then(data => {
 console.log(data.length)
         if (data.length > 10) {
             countryContainer.innerHTML = ''
@@ -42,6 +40,8 @@ console.log(data.length)
             return  Notify.info("Too many matches found. Please enter a more specific name.")
         } else if (data.length >= 2 && data.length <= 10) {
             countryContainer.innerHTML = ''
+           
+
             return createCountryMarkup(data)
         } else  {
             listEl.innerHTML = ''
